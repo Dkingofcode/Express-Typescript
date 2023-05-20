fc5782e392a082b5a73088d92bb2bb5444f716a5
David <doladepo128@gmail.com>
2023-05-20 08:48:07 +0100

    Creating route handlers

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,37 @@
-import express, {Request, Response} from "express";
+import express, {NextFunction, Request, Response} from "express";
 
 const app = express();
 
 app.use(express.json());
 
 
-app.route("/").get((req: Request, res: Response) => {
-   return res.send("You make a GET request"); 
-})
-.post((req: Request, res: Response) => {
-   return res.send("You make a POST request");
-})
-.put((req: Request, res: Response) => {
-    return res.send("You make a PUT request");
-})
+function handleGetBookOne(req: Request, res: Response, next: NextFunction) {
+ console.log(req.params);
+ 
+ next()
+}
 
+function handleGetBookTwo(req: Request, res: Response, next: NextFunction) {
+   console.log('second handler');
+   return res.send(req.params); 
+}
+
+
+app.get('/api/books/:bookId/:authorId', [handleGetBookOne, handleGetBookTwo])
+// Request Chaining  
+
+// app.route("/").get((req: Request, res: Response) => {
+//    return res.send("You make a GET request"); 
+// })
+// .post((req: Request, res: Response) => {
+//    return res.send("You make a POST request");
+// })
+// .put((req: Request, res: Response) => {
+//     return res.send("You make a PUT request");
+// })
+// .delete((req: Request, res: Response) => {
+//    return res.send("You make a DELETE request");
+// })
 
 app.all('/api/all', (req: Request, res: Response) => {
     return res.sendStatus(200);
