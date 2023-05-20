458e8233523c1f3564a00f6c1f1090a8719943dd
David <doladepo128@gmail.com>
2023-05-20 07:16:06 +0100

    Chaining the api requests together

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("Hello world");
+import express, {Request, Response} from "express";
+
+const app = express();
+
+app.use(express.json());
+
+
+app.route("/").get((req: Request, res: Response) => {
+   return res.send("You make a GET request"); 
+})
+.post((req: Request, res: Response) => {
+   return res.send("You make a POST request");
+})
+.put((req: Request, res: Response) => {
+    return res.send("You make a PUT request");
+})
+
+
+app.all('/api/all', (req: Request, res: Response) => {
+    return res.sendStatus(200);
+})
+
+app.listen(3000, () => {
+    console.log("Application listening at http://localhost:3000")
+})
